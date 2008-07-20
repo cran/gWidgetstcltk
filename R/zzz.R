55a6a358d8cfb10f064bc7d99edddb237705662c
John Verzani <gwidgetsrgtk2@gmail.com>
2008-07-20 00:00:00 +0000

    version 0.0-13

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,12 @@
 .onLoad <- function(libname,pkgname,...) {
   require(methods)
   require(tcltk)
+
+  ## version check
+  if(as.numeric(.Tcl("info tclversion")) < 8.5) {
+    cat("\n\n *** gWidgetstcltk needs tcl/tk version 8.5 or newer ***\n\n")
+  }
+  
   oldClasses =c("tkwin")
   setClass("tcltkObject")
   sapply(oldClasses, function(i) {
